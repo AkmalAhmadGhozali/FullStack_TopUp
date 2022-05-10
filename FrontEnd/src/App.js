22bb03ae564da72938b8468ffa9e605e0a8cc488
AkmalAhmadGhozali <74187938+AkmalAhmadGhozali@users.noreply.github.com>
2022-05-10 14:04:56 +0700

    Add files via upload

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { NavbarComponent } from './Components';
+import { Home, Sukses } from './Pages'
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <NavbarComponent />
+          <main>
+            <Switch> 
+                <Route path="/" component={Home} exact/>
+                <Route path="/sukses" component={Sukses} />
+            </Switch>
+          </main>
+      </BrowserRouter>
+    )
+  }
+}
